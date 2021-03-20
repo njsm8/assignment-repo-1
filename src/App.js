91f151f49b7b69f86bb41802f740a2d9047e781e
Nischit Shetty <njmshetty@gmail.com>
2021-03-20 23:12:19 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import { Button } from '@material-ui/core';
 import './App.css';
-
+import logo from './logo.png';
+import logo1 from './logo1.png';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={logo} className="logo" alt='logo'/>
+        <div className="toolbox">
+          <div className="front">
+          <h1 className="congrats">Congratulations</h1>
+          <img src={logo1} className="card" alt='logo'/>
+          <p className="giftcard">Gift  worth rupees 200</p>
+          </div>
+
+          <div className="back">
+          <label for="phone">Enter your phone number:</label>
+          <input type="tel" id="phone" name="phone" />
+          <Button variant="contained" color="primary">
+            Primary
+          </Button>
+
+          </div>
+          
+        </div>
       </header>
     </div>
   );
